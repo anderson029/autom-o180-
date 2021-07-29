a5466400daba340df7b1572caa58fb7bf7d75cf3
anderson029 <andersonfoliveira@yahoo.com>
2021-07-29 20:44:49 -0300

    teste subida

--- rocklov/features/cadastro.feature
+++ rocklov/features/cadastro.feature
@@ -5,37 +5,28 @@ Funcionalidade: Cadastro
     Quero fazer o meu cadastro no RockLov
     Para que eu possa disponibilizá-los para locação
 
+    Contexto: Página de login
+        Dado que acesso a página de cadastro
+
     @cadastro
     Cenario: Fazer cadastro
 
-        Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro completo
+        Quando submeto o seguinte formulário de cadastro:
+            | nome              | email                    | senha    |
+            | Anderson Oliveira | anderson.teste@teste.com | Senha123 |
         Então sou redirecionado para o Dashboard
 
     @tentativa_cadastro
-    Cenario: Submeter cadastro sem o nome
+    Esquema do Cenario: Tentativa de cadastro
 
-        Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro sem o nome
-        Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
+        Quando submeto o seguinte formulário de cadastro:
+            | nome         | email         | senha         |
+            | <nome_input> | <email_input> | <senha_input> |
+        Então vejo a mensagem de alerta: "<mensagem_output>"
 
-    @tentativa_cadastro
-    Cenario: Submeter cadastro sem o email
-
-        Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro sem o email
-        Então vejo a mensagem de alerta: Oops. Informe um email válido!
-
-    @tentativa_cadastro
-    Cenario: Submeter cadastro com email incorreto
-
-        Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro com email incorreto
-        Então vejo a mensagem de alerta: Oops. Informe um email válido!
-
-    @tentativa_cadastro
-    Cenario: Submeter cadastro sem a senha
-
-        Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro sem a senha
-        Então vejo a mensagem de alerta: Oops. Informe sua senha secreta!
+        Exemplos:
+            | nome_input        | email_input        | senha_input | mensagem_output                  |
+            |                   | anderson@gmail.com | Senha123    | Oops. Informe seu nome completo! |
+            | Anderson Oliveira | anderson#gmail.com | Senha123    | Oops. Informe um email válido!   |
+            | Anderson Oliveira |                    | Senha123    | Oops. Informe um email válido!   |
+            | Anderson Oliveira | anderson@gmail.com |             | Oops. Informe sua senha secreta! |
--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -2,58 +2,25 @@ Dado('que acesso a página de cadastro') do
     visit "http://rocklov-web:3000/signup"
 end
   
-Quando('submeto o meu cadastro completo') do
-    #chamando a classe que acessa banco de dados e método que deleta email
-    MongoDB.new.remove_user("anderson.ferias@gmail.com")
+Quando('submeto o seguinte formulário de cadastro:') do |table|
+   
+    user = table.hashes[0]
 
-    find("#fullName").set Faker::Artist.name
-    find("#email").set "anderson.ferias@gmail.com"
-    find("#password").set Faker::Alphanumeric.alpha
+    #chamando a classe que acessa banco de dados e método que deleta email
+    MongoDB.new.remove_user(user[:email])
+
+    find("#fullName").set user[:nome]
+    find("#email").set user[:email]
+    find("#password").set user[:senha]
     click_button "Cadastrar"
-    #@welcome = find("[class=welcome]").text
-end
+  end
   
 Então('sou redirecionado para o Dashboard') do
    #expect(@welcome).to eql ("Let`s Rock Baby")
    expect(page). to have_css ".dashboard"
 end
 
-Quando('submeto o meu cadastro sem o nome') do
-    find("#email").set Faker::Internet.free_email
-    find("#password").set "Senha123"
-    click_button "Cadastrar"
-end
-  
-Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do 
+Então('vejo a mensagem de alerta: {string}') do |expect_alert|
     alert = find(".alert-dark")
-    expect(alert.text).to eql ("Oops. Informe seu nome completo!")
-end
-
-Quando('submeto o meu cadastro sem o email') do
-    find("#fullName").set Faker::Artist.name
-    find("#password").set Faker::Alphanumeric.alpha
-    click_button "Cadastrar"
-end
-
-Quando('submeto o meu cadastro com email incorreto') do
-    find("#fullName").set Faker::Artist.name
-    find("#email").set "anderson*gmail.com"
-    find("#password").set Faker::Alphanumeric.alpha
-    click_button "Cadastrar"
-end
-
-Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
-    alert = find(".alert-dark")
-    expect(alert.text).to eql ("Oops. Informe um email válido!")
-end
-
-Quando('submeto o meu cadastro sem a senha') do
-    find("#fullName").set Faker::Artist.name
-    find("#email").set Faker::Internet.free_email
-    click_button "Cadastrar"
-end
-  
-Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
-    alert = find(".alert-dark")
-    expect(alert.text).to eql ("Oops. Informe sua senha secreta!")
+    expect(alert.text).to eql (expect_alert)
 end
