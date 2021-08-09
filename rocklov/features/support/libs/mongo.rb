b9659561ed3594a41943122a6041a434e5c757b4
anderson029 <andersonfoliveira@yahoo.com>
2021-08-09 07:56:05 -0300

    ajuste

--- rocklov/features/support/libs/mongo.rb
+++ rocklov/features/support/libs/mongo.rb
@@ -4,23 +4,24 @@ Mongo::Logger.logger = Logger.new("./logs/mongo.log")
 
 #Acessando banco de dados
 class MongoDB
-    def remove_user(email)
+
+    def initialize
         client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        users = client[:users]
-        users.delete_many({email: email})    
+        @users = client[:users]
+        @equipos = client[:equipos]
+    end
+
+    def remove_user(email)
+        @users.delete_many({email: email})    
     end
 
     def get_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        users = client[:users]
-        user = users.find({email: email}).first
+        user = @users.find({email: email}).first
         return user[:_id]
     end
 
     def remove_equipo(nome, email)
         user_id = get_user(email)
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-        equipos = client[:equipos]
-        equipos.delete_many({name: nome}, {user: user_id})    
+        @equipos.delete_many({name: nome}, {user: user_id})    
     end
 end
