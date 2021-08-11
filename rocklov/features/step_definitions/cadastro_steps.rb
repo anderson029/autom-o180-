43ba1f840897584430cebf51748eda0656916412
anderson029 <andersonfoliveira@yahoo.com>
2021-08-11 17:11:09 -0300

    implementando padrão page_objects

--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -1,5 +1,5 @@
 Dado('que acesso a página de cadastro') do
-    visit "/signup"
+    @signup_page.open
 end
 
 Quando('submeto o seguinte formulário de cadastro:') do |table|
@@ -8,9 +8,5 @@ Quando('submeto o seguinte formulário de cadastro:') do |table|
 
     #chamando a classe que acessa banco de dados e método que deleta email
     MongoDB.new.remove_user(user[:email])
-
-    find("#fullName").set user[:nome]
-    find("#email").set user[:email]
-    find("#password").set user[:senha]
-    click_button "Cadastrar"
+    @signup_page.create(user)
 end
